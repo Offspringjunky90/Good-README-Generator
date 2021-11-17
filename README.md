7738df28dd2636b94de8566efec9d5df03e1a5e2
Robert Augusta <RobAAugusta@gmail.com>
2021-11-17 16:23:33 -0500

    Final Updates

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Good-README-Generator
+# undefined
+    ![Github licence](http://img.shields.io/badge/license-;lsjdf-blue.svg)
+    
+    ## Description 
+    asdkfj by as'dfj
+    ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    
+    ## Installation 
+    undefined
+    ## Usage 
+    undefined
+    ## License 
+    This project is license under ;lsjdf
+    ## Contributing 
+    undefined
+    ## Tests
+    undefined
+    ## Questions
+    If you have any questions about this projects, please contact me directly at ;lasdjkf. You can view more of my projects at https://github.com/;sakjdf.
+  
---
+++ index.js
@@ -0,0 +1,86 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateReadMe = require('./Assets/genReadMe');
+
+const questionsEl = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide a description of your project',
+        },
+        {
+            type: 'input',
+            name: 'tableofcontents',
+            message: 'Please provide a table of contents for your project', 
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please provide any installation instructions. Leave blank if none',
+        },
+        {
+            type: 'input',
+            name: 'Usage',
+            message: 'What is this project used for?',
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: 'Please enter your license. Leave blank if not applicable',
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Who are the contributors of this project?',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Is there a test included?',
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'What do I do if I have a question?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your github username?',
+        }
+    ]);
+};
+
+const writeFile = data => {
+    fs.writeFile('README.md', data, err => {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log("Your README has been successfully created!")
+        }
+    })
+};
+
+questionsEl()
+
+.then(answers => {
+    return generateReadMe(answers);
+})
+.then(data => {
+    return writeFile(data);
+})
+.catch(err => {
+    console.log(err)
+})
